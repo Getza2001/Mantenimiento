3870ad1c4a456e0ae503b3d4c40de4396a7be6a6
Dulce Maria Fuentes Espinosa <74284122+Espdul@users.noreply.github.com>
2022-03-03 01:03:46 -0600

    Delete Componentes directory

--- Componentes/ProductoImagen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProductoImagen(props) {
-    return (
-        <div>
-            <img src={props.url} alt={'Imagen de nuestro producto'} className={props.clase}/>
-        </div>
-    )
-}
